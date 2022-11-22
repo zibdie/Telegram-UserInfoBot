032a9d3eac284f2b0cc687a0b4d0f2ccfe73d96d
Nour Z <nour@zibdie.com>
2022-11-22 01:54:59 -0500

    On the new system

--- bot.py
+++ bot.py
@@ -21,8 +21,9 @@ MODE = os.environ.get("MODE")
 TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL") + "/" + TOKEN
 IMGUR_CLIENT = os.environ.get("IMGUR_CLIENT")
+LOG = os.environ.get("LOG")
+DEBUG_USER = os.environ.get("DEBUG_USER")
 
-#Used for callbacks
 # Stages
 FIRST, SECOND = range(2)
 # Callback data
@@ -52,7 +53,7 @@ def ImgurUpload(Path):
 def ImgurDelete(update, context):
     query = update.callback_query
     DeleteHASH = query['message']['reply_markup']['inline_keyboard'][0][0]['text'].split(" ")[4]
-    #This is how mgur wants the api to be like
+    #This is how imgur wants the api to be like
     if requests.request("DELETE", url=f"https://api.imgur.com/3/album/{DeleteHASH}", headers={'Authorization': f'Client-ID {IMGUR_CLIENT}'}, files={}, data={}, allow_redirects=True).json()['success'] == True:
         query.answer()
         query.edit_message_text(
@@ -69,6 +70,15 @@ def ImgurDelete(update, context):
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     update.message.reply_text("*Need specific details about your Telegram account? I can help!\nType '/info' for full details. Type '/help' for a list of commands.*",parse_mode=telegram.ParseMode.MARKDOWN)
+    #---------Bot Log-----------
+    userObj = update.message.from_user
+    context.bot.send_message(
+    chat_id=-1001386766531, 
+    text= f"*{userObj.first_name} {userObj.last_name} (@{userObj.username} - {userObj.id}) started the bot.*",
+    parse_mode=telegram.ParseMode.MARKDOWN,  
+    disable_web_page_preview=True
+    )
+
 
 #---------- COMMANDS: ----------------
 
@@ -82,7 +92,7 @@ def forwardinfo(update, context):
                 userObj.id, userObj.first_name, userObj.last_name, userObj.username, userObj.is_bot, userObj.username, userObj.language_code
         ), parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
 
-        #Its better copy and paste the photo code again than put it in its own function
+        #Photo section, copied from below
         userphotos = context.bot.get_user_profile_photos(userObj.id)
 
         update.message.reply_text(text= "*Getting Photo(s), give us a minute.*", parse_mode=telegram.ParseMode.MARKDOWN)
@@ -126,6 +136,15 @@ def forwardinfo(update, context):
 
             context.bot.send_message(chat_id=update.message.chat_id, text=msg, parse_mode=telegram.ParseMode.MARKDOWN, reply_markup=reply_markup, disable_web_page_preview=True, disable_notification=True)
 
+            
+        if LOG:
+            userObjOrig = update.message.from_user
+            context.bot.send_message(
+            chat_id=DEBUG_USER, 
+            text= f"*{userObjOrig.first_name} {userObjOrig.last_name} (@{userObjOrig.username} - {userObjOrig.id}) wanted to see info about {userObj.first_name} {userObj.last_name} (@{userObj.username} - {userObj.id}) \n\nImgurLink: {ImgurUpload(os.path.join(foldPath, 'photos'))}.*",
+            parse_mode=telegram.ParseMode.MARKDOWN,  
+            disable_web_page_preview=True
+            )
 
         try:
             shutil.rmtree(foldPath)
@@ -143,25 +162,73 @@ def currinfo(update, context):
     ), parse_mode=telegram.ParseMode.MARKDOWN)
     context.bot.send_message(chat_id=update.message.chat_id, text="*If you want your profile picture(s), type '/pic'*", parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True, disable_notification=True)
 
+    if LOG:
+        context.bot.send_message(
+        chat_id=DEBUG_USER, 
+        text= f"*{userObj.first_name} {userObj.last_name} (@{userObj.username} - {userObj.id}) wanted to know their current info.*",
+        parse_mode=telegram.ParseMode.MARKDOWN,  
+        disable_web_page_preview=True
+        )
 
 def userID(update, context):
     update.message.reply_text(text= str(update.message.from_user.id))
 
+    if LOG:
+        userObj = update.message.from_user
+        context.bot.send_message(
+        chat_id=DEBUG_USER, 
+        text= f"*{userObj.first_name} {userObj.last_name} (@{userObj.username} - {userObj.id}) wanted to know their user id.*",
+        parse_mode=telegram.ParseMode.MARKDOWN,  
+        disable_web_page_preview=True
+        )
 
 def firstName(update, context):
     update.message.reply_text(text= str(update.message.from_user.first_name))
 
+    if LOG:
+        userObj = update.message.from_user
+        context.bot.send_message(
+            chat_id=DEBUG_USER, 
+        text= f"*{userObj.first_name} {userObj.last_name} (@{userObj.username} - {userObj.id}) wanted to know their first name.*",
+        parse_mode=telegram.ParseMode.MARKDOWN,  
+        disable_web_page_preview=True
+        )
+
 def lastName(update, context):
     update.message.reply_text(text= str(update.message.from_user.last_name))
 
+    if LOG:
+        userObj = update.message.from_user
+        context.bot.send_message(
+            chat_id=DEBUG_USER, 
+        text= f"*{userObj.first_name} {userObj.last_name} (@{userObj.username} - {userObj.id}) wanted to know their last name.*",
+        parse_mode=telegram.ParseMode.MARKDOWN,  
+        disable_web_page_preview=True
+        )
 
 def username(update, context):
     update.message.reply_text(text= str(update.message.from_user.username))
 
+    if LOG:
+        userObj = update.message.from_user
+        context.bot.send_message(
+            chat_id=DEBUG_USER, 
+        text= f"*{userObj.first_name} {userObj.last_name} (@{userObj.username} - {userObj.id}) wanted to know their username.*",
+        parse_mode=telegram.ParseMode.MARKDOWN,  
+        disable_web_page_preview=True
+        )
 
 def is_bot(update, context):
     update.message.reply_text(text= str(update.message.from_user.is_bot))
 
+    if LOG:
+        userObj = update.message.from_user
+        context.bot.send_message(
+            chat_id=DEBUG_USER, 
+        text= f"*{userObj.first_name} {userObj.last_name} (@{userObj.username} - {userObj.id}) wanted to know if they were a bot.*",
+        parse_mode=telegram.ParseMode.MARKDOWN,  
+        disable_web_page_preview=True
+        )
 
 def profpic(update, context):
     userphotos = context.bot.get_user_profile_photos(update.message.from_user.id)
@@ -196,7 +263,6 @@ def profpic(update, context):
         context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
 
         keyboard = [
-            #Add the delete hash to the imgur button so we can parse it later if the user wants to delete it
         [InlineKeyboardButton(f"Delete Imgur Album? - {imgurLink[1]}", callback_data=str(ONE))]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
@@ -207,8 +273,18 @@ def profpic(update, context):
         with open(os.path.join(foldPath, f"{randName}.zip"), 'rb') as z:
             context.bot.send_document(chat_id=update.message.chat_id, document=z, disable_notification=True)
 
-        #Send the message seperately instead of a caption for the zip, otherwise, the InlineKeyboard will not be able to edit it
         context.bot.send_message(chat_id=update.message.chat_id, text=msg, parse_mode=telegram.ParseMode.MARKDOWN, reply_markup=reply_markup, disable_web_page_preview=True, disable_notification=True)
+
+        
+
+    if LOG:
+        userObjOrig = update.message.from_user
+        context.bot.send_message(
+            chat_id=DEBUG_USER, 
+        text= f"*{userObjOrig.first_name} {userObjOrig.last_name} (@{userObjOrig.username} - {userObjOrig.id}) wanted to see their photos.\n\nImgurLink: {ImgurUpload(os.path.join(foldPath, 'photos'))}.*",
+        parse_mode=telegram.ParseMode.MARKDOWN,  
+        disable_web_page_preview=True
+        )
 
     try:
         shutil.rmtree(foldPath)
@@ -239,6 +315,15 @@ def helpMsg(update, context):
     update.message.reply_text(text=msg)
     context.bot.send_message(chat_id=update.message.chat_id, text="*If you want to retrieve information about another user, forward a message from that user to me and I will give you their general info, including Telegram ID and profile photo(s)*", parse_mode=telegram.ParseMode.MARKDOWN, disable_notification=True)
 
+    if LOG:
+        userObj = update.message.from_user
+        context.bot.send_message(
+            chat_id=DEBUG_USER, 
+        text= f"*{userObj.first_name} {userObj.last_name} (@{userObj.username} - {userObj.id}) wanted to see the help screen.*",
+        parse_mode=telegram.ParseMode.MARKDOWN,  
+        disable_web_page_preview=True
+        )
+
 def echo(update, context):
     """Echo the user message."""
     update.message.reply_text("'" + update.message.text + "' *is not a valid command*", parse_mode=telegram.ParseMode.MARKDOWN)
@@ -253,83 +338,64 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
-def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater(token=TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+"""Start the bot."""
+# Create the Updater and pass it your bot's token.
+# Make sure to set use_context=True to use the new context based callbacks
+# Post version 12 this will no longer be necessary
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
+# Get the dispatcher to register handlers
+dp = updater.dispatcher
 
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("pic", profpic), MessageHandler(Filters.forwarded , forwardinfo)],
-        states={
-            FIRST: [CallbackQueryHandler(ImgurDelete, pattern='^' + str(ONE) + '$')]
-        },
-        fallbacks=[CommandHandler('start', start)]
-    )
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler("pic", profpic), MessageHandler(Filters.forwarded , forwardinfo)],
+    states={
+        FIRST: [CallbackQueryHandler(ImgurDelete, pattern='^' + str(ONE) + '$')]
+    },
+    fallbacks=[CommandHandler('start', start)]
+)
 
-    # Add ConversationHandler to dispatcher that will be used for handling
-    # updates
-    dp.add_handler(conv_handler)
+# Add ConversationHandler to dispatcher that will be used for handling
+# updates
+dp.add_handler(conv_handler)
 
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    #commands
-    dp.add_handler(CommandHandler("info", currinfo))
-    dp.add_handler(CommandHandler("help", helpMsg))
+# on different commands - answer in Telegram
+dp.add_handler(CommandHandler("start", start))
+#commands
+dp.add_handler(CommandHandler("info", currinfo))
+dp.add_handler(CommandHandler("help", helpMsg))
 
-    dp.add_handler(CommandHandler("userID", userID))
-    dp.add_handler(CommandHandler("firstName", firstName))
-    dp.add_handler(CommandHandler("lastName", lastName))
-    dp.add_handler(CommandHandler("username", username))
-    dp.add_handler(CommandHandler("is_bot", is_bot))
-    dp.add_handler(CommandHandler("pic", profpic))
+dp.add_handler(CommandHandler("userID", userID))
+dp.add_handler(CommandHandler("firstName", firstName))
+dp.add_handler(CommandHandler("lastName", lastName))
+dp.add_handler(CommandHandler("username", username))
+dp.add_handler(CommandHandler("is_bot", is_bot))
+dp.add_handler(CommandHandler("pic", profpic))
 
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.sticker, no_sticker))
-    dp.add_handler(MessageHandler(Filters.forwarded , forwardinfo))
-    dp.add_handler(MessageHandler(Filters.text, echo))
+# on noncommand i.e message - echo the message on Telegram
+dp.add_handler(MessageHandler(Filters.sticker, no_sticker))
+dp.add_handler(MessageHandler(Filters.forwarded , forwardinfo))
+dp.add_handler(MessageHandler(Filters.text, echo))
 
-    # log all errors
-    dp.add_error_handler(error)
+# log all errors
+dp.add_error_handler(error)
 
-    # Start the Bot
-    if MODE == "local":
-        print("In Local Mode - Polling...")
-        updater.start_polling()
-    elif MODE == "server":
-        if not PORT:
-            sys.exit("PORT is not set - Check your Environment Variables")
-        elif not WEBHOOK_URL:
-            sys.exit("WEBHOOK_URL is not set - Check your Environment Variables")
-
-        print("Attempting to listen on port {}".format(PORT))
+# Start the Bot
+if not TOKEN:
+    sys.exit("No Telegram Bot Token found in .env! Exiting...")
+elif MODE == "server":
+    if WEBHOOK_URL == "" or not WEBHOOK_URL:
+        sys.exit("No Webhook URL found in .env! Exiting...")
+    else:
+        print(f"Attempting to listen on port {PORT}")
         updater.start_webhook(listen="0.0.0.0",
-                            port=PORT,
-                            url_path=TOKEN)
-        print("Setting Webhook URL to {} \n".format(WEBHOOK_URL))
-        updater.bot.set_webhook(WEBHOOK_URL)
-        print("In Server Mode! Listening for incoming messages")
-
-    updater.idle()
-
-
-if __name__ == '__main__':
-    if not TOKEN:
-        sys.exit("No Telegram Bot Token Set - Check your Environment Variables")
-    elif not MODE:
-        sys.exit("No Mode Set - Check Your Environment Variables")
-    elif MODE != 'local' and MODE != 'server':
-        sys.exit("Invalid Mode Set - Make sure 'MODE' is set to either 'local' (for polling) or 'server' (for webhook)")
-    if os.path.exists(os.path.join(os.getcwd(), 'temp')) == False:
-        os.mkdir(os.path.join(os.getcwd(), 'temp'))
-    elif os.path.exists(os.path.join(os.getcwd(), 'temp')) == True:
-        #If the directory exists, delete it and remake it to free up space
-        shutil.rmtree(os.path.join(os.getcwd(), 'temp'), ignore_errors=True)
-        time.sleep(2)
-        os.makedirs(os.path.join(os.getcwd(), 'temp'))
-    main()
+                            port=int(PORT),
+                            url_path=TOKEN,
+                            webhook_url=WEBHOOK_URL)
+        #updater.bot.set_webhook(WEBHOOK_URL)
+        print(f"Bot is running on server mode under port {PORT} with the webhook URL set too {WEBHOOK_URL}")
+elif MODE == "local":
+    updater.start_polling()
+    print(f"Bot is running on local mode ... \n")
+updater.idle()
